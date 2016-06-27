bc7b2f00719b69c8a8cd35c51f74bfc812364d5b
ajitavdutta <ajitav.dutta@gmail.com>
2016-06-27 22:23:34 +0530

    Initial Version.
    Main Screen changes

--- UTLogUtility/src/com/app/utlog/Main.java
+++ UTLogUtility/src/com/app/utlog/Main.java
@@ -1,19 +1,29 @@
 package com.app.utlog;
 	
 import javafx.application.Application;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("RootScreen.fxml"));
-			Scene scene = new Scene(root,400,400);
+			// Get the current monitor screen dimension
+			Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+			
+			//Step 1 - Load the AnchorPane (Main Screen)
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("screens/RootScreen.fxml"));
+			
+			//Step 2 - Setup the scene and add default style CSS
+			Scene scene = new Scene(root,primaryScreenBounds.getWidth(),primaryScreenBounds.getHeight());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setTitle("Unit Test Log Utility");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
