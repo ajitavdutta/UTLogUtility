af0edc33f8eb52a135c945541f41605f016e985d
ajitavdutta <ajitav.dutta@gmail.com>
2016-06-27 23:16:21 +0530

    Main.java updated to allow Utility to be displayed in MacOS properly.

--- UTLogUtility/src/com/app/utlog/Main.java
+++ UTLogUtility/src/com/app/utlog/Main.java
@@ -20,14 +20,16 @@ public class Main extends Application {
 			//Step 1 - Load the AnchorPane (Main Screen)
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("screens/RootScreen.fxml"));
 			AnchorPane root = (AnchorPane)loader.load();
-			
+			System.out.println(primaryScreenBounds.getWidth() + "X" + primaryScreenBounds.getHeight());
 			//Step 2 - Setup the scene and add default style CSS
 			Scene scene = new Scene(root,primaryScreenBounds.getWidth(),primaryScreenBounds.getHeight());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			primaryStage.setTitle("Unit Test Log Utility");
 			primaryStage.getIcons().add(new Image("file:resources/images/UTApp_Main_Icon.png"));
-			primaryStage.setMaximized(true);
+			// setMaximized does not show the screen for Mac.
+			// not requires as we are setting the Stage to cover the entire screen.
+			// primaryStage.setMaximized(true);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
