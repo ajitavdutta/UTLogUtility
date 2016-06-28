a7a94e94c33bb31b2bc92e0adcc2a8d6fc5caf3f
ajitavdutta <ajitav.dutta@gmail.com>
2016-06-28 22:26:01 +0530

    Fix setMaximized issue with Mac OS X

--- UTLogUtility/src/com/app/utlog/Main.java
+++ UTLogUtility/src/com/app/utlog/Main.java
@@ -17,19 +17,27 @@ public class Main extends Application {
 			// Get the current monitor screen dimension
 			Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 			
-			//Step 1 - Load the AnchorPane (Main Screen)
+			// Step 1 - Load the AnchorPane (Main Screen)
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("screens/RootScreen.fxml"));
 			AnchorPane root = (AnchorPane)loader.load();
-			System.out.println(primaryScreenBounds.getWidth() + "X" + primaryScreenBounds.getHeight());
-			//Step 2 - Setup the scene and add default style CSS
+			
+			// Display the screen size or resolution of the current display.
+			// System.out.println(primaryScreenBounds.getWidth() + "X" + primaryScreenBounds.getHeight());
+			
+			// Step 2 - Setup the scene and add default style CSS
 			Scene scene = new Scene(root,primaryScreenBounds.getWidth(),primaryScreenBounds.getHeight());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			primaryStage.setTitle("Unit Test Log Utility");
 			primaryStage.getIcons().add(new Image("file:resources/images/UTApp_Main_Icon.png"));
-			// setMaximized does not show the screen for Mac.
-			// not requires as we are setting the Stage to cover the entire screen.
-			// primaryStage.setMaximized(true);
+			String os = System.getProperty("os.name");
+			
+			// setMaximized does not work in Mac OS X
+			// So setMaximized will be executed for non Mac OS.
+			if (!os.contains("Mac")){
+				primaryStage.setMaximized(true);
+			}
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- UTLogUtility/src/com/app/utlog/screens/RootScreenController.java
+++ UTLogUtility/src/com/app/utlog/screens/RootScreenController.java
@@ -1,5 +1,16 @@
 package com.app.utlog.screens;
 
-public class RootScreenController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class RootScreenController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
